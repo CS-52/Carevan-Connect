eb47c3c218242e91c8b3430bb9f6a9cc472fef40
Joyce Xu <joycexu@DN0a62430c.SUNet>
2018-04-24 16:39:58 -0700

    Add map carevan marker and popup

--- src/map.js
+++ src/map.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import ReactHoverObserver from 'react-hover-observer';
+import './map.css'
 
 const AnyReactComponent = ({ text }) => <div><h1>{text}</h1></div>;
+
+const CarevanMarker = ({ imgSrc, isHovering = false }) => 
+  <div>
+    <img src={require("" + imgSrc)} alt="carevan marker" style={{width: "50px"}}/>
+    {isHovering ? 
+      <div className="popup-box">
+        <h6>Resources Available</h6>
+        <p>glasses: 5</p>
+        <p>socks: 9</p>
+        <p>haircut vouchers: 30</p>
+      </div>
+      : null
+    }
+  </div>
+
 
 class SimpleMap extends Component {
   static defaultProps = {
     center: {
-      lat: 37.75,
+      lat: 37.755,
       lng: -122.44
+    },
+
+    carevan: {
+      lat: 37.7791,
+      lng: -122.4158,
+      imgSrc: "./imgs/caravan.png"
     },
     zoom: 13
   };
@@ -21,11 +44,14 @@ class SimpleMap extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <AnyReactComponent
-            lat={37.8}
-            lng={-122.45}
-            text={''} 
-          />
+          <ReactHoverObserver 
+            lat={this.props.carevan.lat}
+            lng={this.props.carevan.lng}>
+            
+            <CarevanMarker    
+              imgSrc={this.props.carevan.imgSrc}
+            />
+          </ReactHoverObserver>
         </GoogleMapReact>
       </div>
     );
